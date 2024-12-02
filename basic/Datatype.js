8275c0eddc788da9611aa4d636f01886c946406d
Sarswoti Bhandari <bhandarisarswoti33@gmail.com>
2024-12-02 04:23:09 +0000

    datatypes

---
+++ basic/Datatype.js
@@ -0,0 +1,20 @@
+let Name = "sarswoti bhandari"
+let age = 22
+let address = "kanchnapur"
+let phoneNo =9868571522
+console.table([Name,age,address,phoneNo])
+console.log(typeof "age")
+
+
+const dogs = [
+    { name: "Yoshi", color: "Black", personality: "Calm" },
+    { name: "Melanie", color: "Brown", personality: "Hyperactive" },
+    { name: "Peppers", color: "white", personality: "Unpredictable" },
+  ];
+  
+  console.table(dogs);
+  const biggestNum = Number.MAX_VALUE;
+const smallestNum = Number.MIN_VALUE;
+const infiniteNum = Number.POSITIVE_INFINITY;
+const negInfiniteNum = Number.NEGATIVE_INFINITY;
+const notANum = Number.NaN;
