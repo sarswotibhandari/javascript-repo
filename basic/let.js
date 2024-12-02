d2a98e392d0c11c2d3d2685cff394c2e867e4df8
Sarswoti Bhandari <bhandarisarswoti33@gmail.com>
2024-12-02 03:59:40 +0000

    let-const-var

---
+++ basic/let.js
@@ -0,0 +1,7 @@
+const accountId = 1233
+let accountEmail ="bhandarisarswoti33@gmail.com"
+let fullName ="Sarswoti bhandari"
+var addressName="Kanchnapur"
+var accountPassword ="9067"
+console.table([accountId,accountEmail, fullName,addressName,accountPassword])
+console .log (typeof "accountId");
