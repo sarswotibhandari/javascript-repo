3f473396dcc1e97bb78c5b5b6052c56342e7894e
Sarswoti Bhandari <bhandarisarswoti33@gmail.com>
2024-12-03 16:52:00 +0000

    datetime in js

---
+++ basic/datetime.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate);
+  console.log(myDate.toString());
+  console.log(myDate.toDateString());
+  console.log (myDate.toLocaleString());
+
+
+
+  
+  let myCreatedDate = new Date(2024,5,5,6,7)
+  console.log(myCreatedDate.toDateString());
+  console.log(myCreatedDate.toLocaleString());
+
