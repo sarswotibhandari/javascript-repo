16bf7cc6ddc9dea8b03ed75f9805d2a89fe71a67
Sarswoti Bhandari <bhandarisarswoti33@gmail.com>
2024-12-03 17:17:31 +0000

    array in js

---
+++ basic/array.js
@@ -0,0 +1,11 @@
+let newArray = new Array (0,1,2,3,4,5,6,7,8,9)
+console.log(newArray)
+newArray.push(10)
+
+console.log(newArray)
+console.log (newArray.includes(10))
+console.log (newArray.indexOf(7))
+const anotherArray =  [1,2,[4,5],7,[8],9,0]
+const myArray = anotherArray.flat(Infinity)
+console.log (myArray)
+
